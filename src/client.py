098d8a8331f032fc18542a0194790cbd617fd8d9
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-07-27 21:08:57 +0100

    added error handling

--- src/client.py
+++ src/client.py
@@ -8,7 +8,10 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.connect((ip_address, port))
 
 while True:
-    command_input = input()
-    server_socket.send(command_input.encode())
-    data = server_socket.recv(1024)
-    print(data.decode())
+    command_input = input("Please input a command: ")
+    try:
+        server_socket.send(command_input.encode())
+        data = server_socket.recv(1024)
+        print(data.decode())
+    except Exception as e:
+        print(f"Error encountered: {e}")
